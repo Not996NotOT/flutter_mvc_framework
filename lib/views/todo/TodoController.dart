6edf90778dfdd4a637570e3d9e1d73e92739920f
zhangxing <zhangx@tongfutele.com>
2021-05-21 21:39:05 +0800

    2021-05-21

--- lib/views/todo/TodoController.dart
+++ lib/views/todo/TodoController.dart
@@ -1,18 +1,29 @@
 import 'package:flutter_mvc_framework/models/viewmodels/TodoViewModel.dart';
 import 'package:get/get.dart';
+import 'package:rxdart/rxdart.dart';
 
 class TodoController extends GetxController {
   var count = 0.obs;
-  RxList<TodoViewModel> list = new RxList<TodoViewModel>();
+  var _subject = new ReplaySubject();
+  var list = new RxList<TodoViewModel>();
   @override
   void onInit() {
     super.onInit();
+    _subject.throttleTime(Duration(seconds: 1)).listen((event) {
+      list.add(new TodoViewModel(id: list.length, todo: "todo${list.length}"));
+    });
+
     List.generate(
         5,
         (index) =>
             list.add(new TodoViewModel(id: index, todo: "todo${index}")));
   }
 
-  void addTodo() =>
-      list.add(new TodoViewModel(id: list.length, todo: "todo${list.length}"));
+  @override
+  void dispose() {
+    super.dispose();
+    _subject.close();
+  }
+
+  void addTodo() => _subject.add(0);
 }
